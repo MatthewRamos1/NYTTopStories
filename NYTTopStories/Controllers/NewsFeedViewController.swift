ada6a162b0a42475df0b85617ab67d871f1a52de
Matthew Ramos <mmramos309@gmail.com>
2020-02-06 11:40:01 -0500

    Added collection view and search bar to news feed controller

--- NYTTopStories/Controllers/NewsFeedViewController.swift
+++ NYTTopStories/Controllers/NewsFeedViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class NewsFeedViewController: UIViewController {
+    
+    private let newsFeedView = NewsFeedView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        newsFeedView.collectionView.dataSource = self
+        newsFeedView.collectionView.delegate = self
+        
+        newsFeedView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "articleCell")
+        
 
+    }
+    
+    override func loadView() {
+        view = newsFeedView
     }
 
 
+}
+
+extension NewsFeedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "articleCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+extension NewsFeedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 30
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
 }
